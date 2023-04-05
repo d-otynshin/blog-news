e26e031179afe4b59a3a0db3131bc85ce019e87a
Daniyar Otynshin <daniyar.otynshin@gmail.com>
2023-04-06 03:07:59 +0600

    feature: install loki and add github actions

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,22 +4,22 @@ import { BuildEnv, BuildPaths } from './config/build/types/config';
 import path from 'path';
 
 export default (env: BuildEnv): webpack.Configuration => {
-	const paths: BuildPaths = {
-		entry:  path.resolve(__dirname, 'src', 'index.tsx'),
-		build: path.resolve(__dirname, 'build'),
-		html: path.resolve(__dirname, 'public', 'index.html'),
-		src: path.resolve(__dirname, 'src'),
-	}
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src')
+  }
 
-	const mode = env.mode || 'development';
-	const PORT = env.port || 3000;
-	const isDevelopment = mode === 'development';
+  const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+  const isDevelopment = mode === 'development';
 
-	return buildWebpackConfig({
-		mode,
-		paths,
-		port: PORT,
-		open: true,
-		isDevelopment
-	});
+  return buildWebpackConfig({
+    mode,
+    paths,
+    port: PORT,
+    open: true,
+    isDevelopment
+  });
 };
